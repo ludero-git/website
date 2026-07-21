c0cce6f4f91848aeae36448d4db8839c2bf689b6
Melchior <64213755+MelchMwoan@users.noreply.github.com>
2026-07-21 16:21:10 +0200

    Added Altcha captcha mechanism

---
+++ src/Ludero.Web/Services/MemoryCaptchaStore.cs
@@ -0,0 +1,22 @@
+﻿using Ixnas.AltchaNet;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Ludero.Web.Services
+{
+    public class MemoryCaptchaStore(IMemoryCache cache) : IAltchaChallengeStore
+    {
+        private readonly IMemoryCache _cache = cache;
+
+        public Task Store(string challenge, DateTimeOffset expiryUtc)
+        {
+            _cache.Set(challenge, true, expiryUtc);
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> Exists(string challenge)
+        {
+            var exists = _cache.TryGetValue(challenge, out _);
+            return Task.FromResult(exists);
+        }
+    }
+}
